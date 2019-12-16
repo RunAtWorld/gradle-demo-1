ad7c674a429e159555d88a6fa2e55364a3a081fd
LiPF <hudalpf@163.com>
2019-12-16 22:12:33 +0800

    gradle jackson demo

---
+++ src/main/java/dev/lpf/json/entity/Friend.java
@@ -0,0 +1,14 @@
+package dev.lpf.json.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Friend {
+    private String nickname;
+    private int age;
+
+}
